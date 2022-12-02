cbdff765eafff1579e340c1119e67f22ad3c621d
Andrei Diaconescu <andreidiaconescu2003@gmail.com>
2022-12-02 19:22:12 +0200

    bug fixes

--- scripts/script.js
+++ scripts/script.js
@@ -70,7 +70,7 @@
 	
 	//Initializam plugin-ul pentru harta
     var mymap = L.map('mapid').setView([30, -31], 1);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         minZoom: 1,
         id: 'mapbox.streets'
     }).addTo(mymap);
